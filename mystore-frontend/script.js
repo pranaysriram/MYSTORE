e6dc8c2e37dc50f0bb6f578e46479d75b85e8538
pranaysriram <162683217+pranaysriram@users.noreply.github.com>
2025-09-13 17:39:31 +0530

    Add files via upload

---
+++ mystore-frontend/script.js
@@ -0,0 +1,20 @@
+async function loadProducts() {
+  const res = await fetch("http://localhost:5000/api/products");
+  const products = await res.json();
+
+  const container = document.getElementById("products");
+  container.innerHTML = "";
+
+  products.forEach(p => {
+    container.innerHTML += `
+      <div class="card">
+        <img src="${p.image}" alt="${p.name}">
+        <h3>${p.name}</h3>
+        <p>₹${p.price}</p>
+        <p>${p.available ? "✅ Available" : "❌ Out of Stock"}</p>
+      </div>
+    `;
+  });
+}
+
+loadProducts();
